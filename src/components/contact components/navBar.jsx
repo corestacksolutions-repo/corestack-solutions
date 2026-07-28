3322529a7f5d21f855b9bc19fb1947081974b4ac
Ishmael-smart-kaunda <ishmaelsmart003@gmail.com>
2026-07-27 23:08:53 -0700

    sms-2

--- src/components/contact components/navBar.jsx
+++ src/components/contact components/navBar.jsx
@@ -517,7 +517,7 @@ const Navbar = () => {
                  </NavLink>
                </ul>
               
-               <a href="sms:+265887914433&body=Hello%20CoreStack"
+               <a href="sms:+265887914433"
                   className="
                      self-center
                      mt-16
