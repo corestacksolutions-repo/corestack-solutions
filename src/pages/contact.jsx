4e2f5ef85ebeb5f316a2766abd940dfb98605cd2
Ishmael-smart-kaunda <ishmaelsmart003@gmail.com>
2026-07-28 11:57:54 -0700

    removed social from contact page

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -2,7 +2,7 @@ import Nav from "../components/contact components/navBar";
 import ContactInfor from "../components/contact components/contack info";
 import Hero from "../components/contact components/hero";
 import Container from "../components/contact components/container";
-import Socials from "../components/contact components/social";
+
 
 
 
